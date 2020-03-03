cfc937dd4e933820ff3d85a4bc66f4ea64073b15
sakeerin <theblue0800@gmail.com>
2020-03-03 17:27:32 +0800

    coding in part display calendar

--- meeting-room/backend/modules/meeting/controllers/DefaultController.php
+++ meeting-room/backend/modules/meeting/controllers/DefaultController.php
@@ -17,4 +17,22 @@ class DefaultController extends Controller
     {
         return $this->render('index');
     }
+
+    public function actionJsoncalendar($start=NULL,$end=NULL,$_=NULL){
+        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $times = \backend\modules\meeting\models\Meeting::find()->all();
+        $events = [];
+        foreach($times as $time){
+            $Event = new \yii2fullcalendar\models\Event();
+            $Event->id = $time->id;
+            $Event->title = $time->title.'('.$time->room->name.')';
+            $Event->start = $time->date_start;
+            $Event->end = $time->date_end;
+            $Event->color = $time->room->color;
+            $Event->url = \yii\helpers\Url::to(['/meeting/meeting/view','id' => $time->id]);
+            $events[] = $Event;
+
+        }
+        return $events;
+    }
 }
--- meeting-room/backend/modules/meeting/views/default/index.php
+++ meeting-room/backend/modules/meeting/views/default/index.php
@@ -1,11 +1,29 @@
 <?php
 use yii\helpers\Url;
+use yii\helpers\Html;
+
+$this->title = 'การจองห้องประชุม';
 ?>
-<?= yii2fullcalendar\yii2fullcalendar::widget([
-      'options' => [
+<div class="box box-info box-solid">
+    <div class="box-header">
+        <h3 class="box-title"><i class="fa fa-calendar-check-o"></i> <?= Html::encode($this->title) ?></h3>
+    </div>
+    <div class="box-body">
+        <?= yii2fullcalendar\yii2fullcalendar::widget([
+    'header' => [
+        'left' => 'prev,next today',
+        'center' => 'title',
+        'right' => 'month,agendaWeek,agendaDay'
+    ],
+    'clientOptions' => [
         'lang' => 'th',
-        //... more options to be defined here!
-      ],
-      'events' => Url::to(['/timetrack/default/jsoncalendar'])
+        'lazyFecthing' => true,
+        'timeFormat' => 'H:mm',
+        'forceEventDuration' => true,
+        'ignoreTimezone' => true,
+    ],
+    'events' => Url::to(['/meeting/default/jsoncalendar'])
     ]);
 ?>
+    </div>
+</div>
